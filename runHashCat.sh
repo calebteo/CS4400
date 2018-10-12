5fa56a4ed6825b3ed3997bdbab7cc6e3408e62d9
Ubuntu <ubuntu@ip-172-31-17-170.us-east-2.compute.internal>
2018-10-12 23:49:01 +0000

    Some SHA512

--- runHashCat.sh
+++ runHashCat.sh
@@ -1,1 +1,1 @@
-hashcat -a 0 -m 7400 ~/code/Crack2/SHA256.hashes ~/crunch-3.6/5-6w.txt -o ~/code/Crack2/SHA256.broken
+hashcat -m 1800 -O ~/code/Crack2/SHA512.hashes -o ~/code/Crack2/SHA512.broken -a 3 -1 ?l -2 ?l  ?1?2?2?2?2
---
+++ runHashCatRock.sh
@@ -0,0 +1,1 @@
+hashcat -m 7400 -O ~/code/Crack2/SHA256.hashes -o ~/code/Crack2/SHA256-R.broken -a 0 ~/rockyou.txt
