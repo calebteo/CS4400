069f63a17a81eed17c35970a2a467b589878ed0d
Caleb Teo <calebteo@Calebs-MacBook-Pro.local>
2018-11-02 12:55:23 +0000

    Fixed argon format, problem with last password (removed comma BUG)

--- Crack3/separete_passwords.sh
+++ Crack3/separete_passwords.sh
@@ -1,9 +1,10 @@
 jq .hashes layer2.json > hashes.txt
-sed 's/[\"\,]//g' hashes.txt > noQuotes.txt
-sed 's/..//' noQuotes.txt > password.hashes
+sed 's/[\"]//g' hashes.txt > noQuotes.txt
+sed 's/.$//g' noQuotes.txt > noCommas.txt
+sed 's/..//' noCommas.txt > password.hashes
 mkdir Output_2
 grep argon2i password.hashes > Output_2/argon2i.hashes
 grep pbkdf2 password.hashes > Output_2/PBKDF2.hashes
 grep round password.hashes > Output_2/SHA512.hashes
 grep sha1 password.hashes > Output_2/SHA1.hashes
-rm hashes.txt noQuotes.txt
+rm hashes.txt noQuotes.txt noCommas.txt
