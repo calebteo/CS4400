0a27b60dcc745aeea6ad9d2c34842300ffa81a63
Caleb-V100 <teoc_cch@v100-1.us-west1-b.c.divine-engine-222621.internal>
2018-11-17 00:14:37 +0000

    Finished

--- runJohn.sh
+++ runJohn.sh
@@ -1,1 +1,3 @@
-~/John/run/john --format=argon2  ~/teamgit/cs7ns1/layer8/argon/argonaa --session=argoin2 --crack-status --progress-every=900 
+../John/run/john --format=PBKDF2-HMAC-SHA256-opencl ~/team13/layer10/layer10.hashes --devices=0,1,2,3 --fork=4 --pot=../pb.pot --wordlist=../DantesWordlist.txt --progress-every=300
+../John/run/john --format=sha1crypt-opencl ~/team13/layer10/layer10.hashes --devices=0,1,2,3 --fork=4 --pot=../sha1crypt_10.pot --wordlist=../DantesWordlist.txt --progress-every=300
+../John/run/john --format=sha512crypt-opencl ~/team13/layer10/layer10.hashes --devices=0,1,2,3 --fork=4 --pot=../sha512.pot --wordlist=../DantesWordlist.txt --progress-every=600
